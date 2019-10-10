138aeb412c49579c3dd7e45fc89f9f09b7d3af09
vedant <vedant.yadav2k@gmail.com>
2019-10-10 16:05:59 +0530

    vedant is cool

---
+++ HelloWorld.py
@@ -0,0 +1,31 @@
+'''n=int(input("---"))
+temp=n
+rev=0
+while(n>0):
+  d=n%10
+  rev=rev*10+d
+  n=n//10
+
+if temp==rev:
+    print("pallindrome")
+elif temp!=rev:
+     print("not") 
+'''
+'''
+q=[1,2,3,4]
+q.append(5)
+print(q)
+q.pop(0) 
+del q
+print(q)'''
+'''
+n=input("___")
+p=reversed(n)  
+if n==p :
+   print("df")
+else :
+  print("asd")   \
+    '''
+a="hello/pes"
+a= a.split("/")
+print(a)
